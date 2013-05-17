e8ba715bb6b50e22e4b1b884cb29c17966c678df
iliqnski <iliqn408@abv.bg>
2013-05-17 11:58:14 +0300

    Added class diagram and Engine.cs unit test class

---
+++ Minesweeper.Tests/EngineTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Minesweeper.Tests
+{
+    [TestClass]
+    public class EngineTests
+    {
+        [TestMethod]
+        public void ParseInputCommandWithValidCommand()
+        {
+           
+        }
+    }
+
+
+}
